42a6816633712ac0965fb786e89d1eea8f4ee63b
Regina Naslund <rnaslund66@gmail.com>
2022-11-21 10:51:00 -0800

    classes exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,73 @@
-﻿namespace Classes
+﻿namespace  Program 
 {
-    public class Program
+    public class Car
     {
-        static void Main(string[] args)
+        public static string model;
+        public static int year;
+        private string v1;
+        private string v2;
+        private int v3;
+
+        public Car()
         {
-            Console.WriteLine("Hello, World!");
+        }
+
+        public Car(string v1, string v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        private static string GetMake()
+        {
+            return GetMake();
+        }
+
+        static void Main(string[] args, string make)
+        {    
+            
+            {
+              var myCar = new Car();
+               
+                   myCar.Make = "Kia";
+                   myCar.Model = "Soul";
+                   myCar.Year = 2014;
+                {
+                    var Ford = new Car();
+                }
+
+                   make = "Ford";
+                   model = "Ranger";
+                   year = 2020;
+                {
+                   
+                    
+                     
+                    
+    };
+                var volkswagen = new Car("Volkswagen", "Atlas", 2022);
+                
+                var carList = new List<Car>();
+                
+                              
+                
+                
+
+
+            }
+            
+              
+            {
+                 
+
+
+
+            }
         }
     }
 }
